cdaff815b75e4b4a21cfea711be7bf0cdcc3431e
OpqaqueYardInfinity <guapitobol2@gmail.com>
2026-07-02 18:13:19 -0600

    important comments deleted

--- src/main/java/com/opaqueyardinfinity/thelostfrontiers/TheLostFrontiersClient.java
+++ src/main/java/com/opaqueyardinfinity/thelostfrontiers/TheLostFrontiersClient.java
@@ -11,9 +11,9 @@ import net.neoforged.neoforge.client.gui.ConfigurationScreen;
 import net.neoforged.neoforge.client.gui.IConfigScreenFactory;
 
 // This class will not load on dedicated servers. Accessing client side code from here is safe.
-@Mod(value = TheLostFrontiers.MODID, dist = Dist.CLIENT)
+@Mod(value = TheLostFrontiers.MOD_ID, dist = Dist.CLIENT)
 // You can use EventBusSubscriber to automatically register all static methods in the class annotated with @SubscribeEvent
-@EventBusSubscriber(modid = TheLostFrontiers.MODID, value = Dist.CLIENT)
+@EventBusSubscriber(modid = TheLostFrontiers.MOD_ID, value = Dist.CLIENT)
 public class TheLostFrontiersClient {
     public TheLostFrontiersClient(ModContainer container) {
         // Allows NeoForge to create a config screen for this mod's configs.
